fbfc3d7dc0195e76f14c651266d8f5cfed6b92fb
Ernest Muriuki <ernest.muriuki@squaddigital.com>
2021-09-10 17:39:17 +0300

    First Commit

---
+++ CovidTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidTracker
+//
+//  Created by ERNEST MURIUKI on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
